40ec09398a31f5c15b410824f2c831e72c3cf72e
andre girao <agirao@unigex.com.br>
2018-03-18 10:18:05 -0300

    adicionado campo complemento nas informações da conta do usuário.

--- truffles-api/src/main/java/br/com/alg/trufflesapi/model/AccountInfo.java
+++ truffles-api/src/main/java/br/com/alg/trufflesapi/model/AccountInfo.java
@@ -32,6 +32,7 @@ public class AccountInfo {
 	private String state;
 	
 	@Column(name="tx_compl")
+	private String complement;
 
 	public Long getId() {
 		return id;
@@ -80,4 +81,75 @@ public class AccountInfo {
 	public void setState(String state) {
 		this.state = state;
 	}
+
+	public String getComplement() {
+		return complement;
+	}
+
+	public void setComplement(String complement) {
+		this.complement = complement;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((addressName == null) ? 0 : addressName.hashCode());
+		result = prime * result + ((addressNumber == null) ? 0 : addressNumber.hashCode());
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((complement == null) ? 0 : complement.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((neighborhood == null) ? 0 : neighborhood.hashCode());
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AccountInfo other = (AccountInfo) obj;
+		if (addressName == null) {
+			if (other.addressName != null)
+				return false;
+		} else if (!addressName.equals(other.addressName))
+			return false;
+		if (addressNumber == null) {
+			if (other.addressNumber != null)
+				return false;
+		} else if (!addressNumber.equals(other.addressNumber))
+			return false;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (complement == null) {
+			if (other.complement != null)
+				return false;
+		} else if (!complement.equals(other.complement))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (neighborhood == null) {
+			if (other.neighborhood != null)
+				return false;
+		} else if (!neighborhood.equals(other.neighborhood))
+			return false;
+		if (state == null) {
+			if (other.state != null)
+				return false;
+		} else if (!state.equals(other.state))
+			return false;
+		return true;
+	}
+	
+	
 }
