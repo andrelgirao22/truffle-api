7b8bdcca3d219e21e487759ae3413c34117dfcd3
Andre Girao <andrelgirao22@hotmail.com>
2021-06-29 11:08:10 -0300

    adicionado novas configuracoes do db

--- src/main/resources/db/migration/V0003__dados_grupos_usuario.sql
+++
@@ -1,9 +0,0 @@
-insert into tb_group (tx_name) value ('ROLE_ADMIN');
-insert into tb_group (tx_name) value ('ROLE_USER');
-insert into tb_group (tx_name) value ('ROLE_DEV');
-
-INSERT INTO `truffleapp`.`tb_account` (`tx_city`, `tx_state`, `bl_status`, `tx_email`, `tx_password`, `tx_name`)
-     VALUES ('Fortaleza', 'CE', true, 'admin@truffles.com.br', '{bcrypt}$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra', 'admin');
-
-INSERT INTO `truffleapp`.`tb_account` (`tx_city`, `tx_state`, `bl_status`, `tx_email`, `tx_password`, `tx_name`)
-     VALUE ('Fortaleza','user@truffles.com.br'	'{bcrypt}$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra');
